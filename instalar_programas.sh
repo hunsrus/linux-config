955e5cfec3d8c36e2b93d977a5ddebf77766e1bb
hunsrus <95552845+hunsrus@users.noreply.github.com>
2021-12-07 23:01:40 -0300

    Add files via upload

---
+++ instalar_programas.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+apt update
+
+#CHROME
+
+#SPOTIFTY
+apt install spotify-client
+
+#VLC
+apt install vlc
+
+#CROSSOVER
+#script, instalar y crackear
+
+#OFFICE 365
+
+#DISCORD
+apt install discord
+
+#STREMIO
+apt isntall stremio
+
+#VIRTUALBOX
+apt install virtualbox
+
+#ARDUINO IDE
+#hacer que se descargue la ultima version de la pagina e instalarla
+
+#GIMP
+apt install gimp
+
+#WINE
+apt install wine
+
+#WINETRICKS
+apt install winetricks
+
+#TRANSMISSION
+apt install transmission
+
+#ONLYOFFICE
+apt install onlyoffice-desktopeditors
+
+#ZOOM
+apt install zoom
+
+#TEAMS
+apt install teams
+
+#OBS
+apt install obs-studio
+
+#MATLAB
+#mmmmmmmmmmmmmmmmmmmmmmmmmm
+
+#PROTEUS
+#ejecutar instalador con wine
+
+#MULTISIM
+#ejecutar instalador con wine
+
+#ISE DS
+#es un viaje, agregar después si se puede automatizar la instalación
+
+#KICAD
+apt install kicad
+
+#QT CREATOR
+#descargar y ejecutar
